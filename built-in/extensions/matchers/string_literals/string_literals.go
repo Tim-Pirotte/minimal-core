adebb168a749f1a33c4413ccfc3acae1e7304c8c
Tim Pirotte <timpirotte@outlook.com>
2026-02-01 17:59:16 +0100

    Adds a state enum for improving the string literal logic

--- built-in/extensions/matchers/string_literals/string_literals.go
+++ built-in/extensions/matchers/string_literals/string_literals.go
@@ -6,7 +6,16 @@ import (
 	"strings"
 )
 
+type state uint
+
+const (
+	outsideString state = iota
+	inString
+)
+
 type StringLiteralMatcher struct {
+	state                state
+
 	stringTt             domain.TokenType
 	interpolatedOpenTt   domain.TokenType
 	interpolatedMiddleTt domain.TokenType
@@ -16,7 +25,7 @@ type StringLiteralMatcher struct {
 }
 
 func NewStringLiteralMatcher(stringTt, interpolatedOpenTt, interpolatedMiddleTt, interpolatedCloseTt domain.TokenType) StringLiteralMatcher {
-	return StringLiteralMatcher{stringTt, interpolatedOpenTt, interpolatedMiddleTt, interpolatedCloseTt, 0}
+	return StringLiteralMatcher{outsideString, stringTt, interpolatedOpenTt, interpolatedMiddleTt, interpolatedCloseTt, 0}
 }
 
 func (s *StringLiteralMatcher) Match(so *tokenizer.Source) (uint, domain.TokenType, string) {
@@ -89,6 +98,121 @@ func (s *StringLiteralMatcher) Match(so *tokenizer.Source) (uint, domain.TokenTy
 			if !ok {
 				// TODO log error
 				panic("Nothing to escape")
+			}
+
+			sb.WriteByte(charToSkip)
+
+			pos += 2
+		} else {
+			sb.WriteByte(currentChar)
+			pos++
+		}
+	}
+}
+
+func (s *StringLiteralMatcher) MatchN(so *tokenizer.Source) (uint, domain.TokenType, string) {
+	switch s.state {
+	case outsideString:
+		return s.lexNewString(so)
+	case inString:
+		return s.lexInterpolatedString(so)
+	default:
+		panic("unreachable")
+	}
+}
+
+func (s *StringLiteralMatcher) lexNewString(so *tokenizer.Source) (uint, domain.TokenType, string) {
+	firstChar, _ := so.Get(0)
+
+	if firstChar != '"' {
+		return 0, domain.IGNORE, ""
+	}
+
+	sb := strings.Builder{}
+
+	pos := 1
+	for {
+		currentChar, ok := so.Get(pos)
+
+		if !ok {
+			// TODO log error
+			panic("expected \" or {")
+		}
+
+		switch currentChar {
+		case '"':
+			return uint(pos) + 1, s.stringTt, sb.String()
+		case '{':
+			s.braceCounter++
+			s.state = inString
+			return uint(pos) + 1, s.interpolatedOpenTt, sb.String()
+		}
+
+		if currentChar == '\\' {
+			sb.WriteByte(currentChar)
+			// The characters that can close a string are all one byte long
+			charToSkip, ok := so.Get(pos + 1)
+
+			if !ok {
+				// TODO log error
+				panic("nothing to escape")
+			}
+
+			sb.WriteByte(charToSkip)
+
+			pos += 2
+		} else {
+			sb.WriteByte(currentChar)
+			pos++
+		}
+	}
+}
+
+func (s *StringLiteralMatcher) lexInterpolatedString(so *tokenizer.Source) (uint, domain.TokenType, string) {
+	firstChar, _ := so.Get(0)
+
+	switch firstChar {
+	case '{':
+		s.braceCounter++
+		return 0, domain.IGNORE, ""
+	case '}':
+		s.braceCounter--
+	default:
+		return 0, domain.IGNORE, ""
+	}
+
+	if s.braceCounter != 0 {
+		return 0, domain.IGNORE, ""
+	}
+
+	sb := strings.Builder{}
+
+	pos := 1
+	for {
+		currentChar, ok := so.Get(pos)
+
+		if !ok {
+			// TODO log error
+			panic("expected \" or {")
+		}
+
+		switch currentChar {
+		case '"':
+			return uint(pos) + 1, s.interpolatedCloseTt, sb.String()
+		case '{':
+			s.braceCounter++
+			s.state = inString
+			return uint(pos) + 1, s.interpolatedMiddleTt, sb.String()
+		}
+
+		if currentChar == '\\' {
+			sb.WriteByte(currentChar)
+			// The characters that can close a string are all one byte long
+			charToSkip, ok := so.Get(pos + 1)
+
+			if !ok {
+				// TODO log error
+				panic("nothing to escape")
 			}
 
 			sb.WriteByte(charToSkip)
--- built-in/extensions/matchers/string_literals/string_literals_test.go
+++ built-in/extensions/matchers/string_literals/string_literals_test.go
@@ -56,3 +56,64 @@ func TestLexUnclosed(t *testing.T) {
 
 	tokenizer.NewTokenizer(config, []byte("\"Hello, world!"))
 }
+
+func TestLexInterpolatedString(t *testing.T) {
+	config := tokenizer.NewTokenizerConfig()
+	stringTt := config.NewTokenType()
+	interpolatedOpenTt := config.NewTokenType()
+	interpolatedMiddleTt := config.NewTokenType()
+	interpolatedCloseTt := config.NewTokenType()
+	stringLiteralMatcher := NewStringLiteralMatcher(stringTt, interpolatedOpenTt, interpolatedMiddleTt, interpolatedCloseTt)
+
+	config.AddMatcher(&stringLiteralMatcher)
+
+	expected := []domain.Token{
+		{Type: interpolatedOpenTt, Value: "Hello, ", Span: domain.Span{Start: 0, Length: 9}},
+		{Type: domain.UNKNOWN, Value: "a", Span: domain.Span{Start: 9, Length: 1}},
+		{Type: interpolatedCloseTt, Value: " world!", Span: domain.Span{Start: 10, Length: 9}},
+		{Type: domain.EOF, Value: "", Span: domain.Span{Start: 19, Length: 0}},
+	}
+
+	actual := tokenizer.NewTokenizer(config, []byte("\"Hello, {a} world!\""))
+
+	i := 0
+	for ; actual.CurrentToken().Type != domain.EOF; i++ {
+		if i >= len(expected) {
+			t.Fatal("Expected", len(expected), "tokens but got", i+1, "tokens")
+		}
+
+		if !reflect.DeepEqual(actual.CurrentToken(), expected[i]) {
+			t.Error("Expected", expected[i], "but got", actual.CurrentToken())
+		}
+
+		actual.Consume()
+	}
+
+	if i+1 != len(expected) {
+		t.Fatal("Expected", len(expected), "tokens but got", i+1, "tokens")
+	}
+}
+
+func TestLexMultipleStringParts(t *testing.T) {
+	
+}
+
+func TestLexMultipleInnerBraces(t *testing.T) {
+	
+}
+
+func TestLexMismatchingOpeningBrackets(t *testing.T) {
+	
+}
+
+func TestLexMismatchingClosingBrackets(t *testing.T) {
+	
+}
+
+func TestLexQuoteInInterpolatedString(t *testing.T) {
+	
+}
+
+func TestLexInterpolatedStringWithoutStartingQuote(t *testing.T) {
+
+}
