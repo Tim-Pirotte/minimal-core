ee43fb0186343a9d51356bd989106c37b4bf7817
Tim Pirotte <timpirotte@outlook.com>
2026-02-01 15:08:39 +0100

    Adds basic string literals

--- built-in/extensions/matchers/string_literals/string_literals.go
+++ built-in/extensions/matchers/string_literals/string_literals.go
@@ -1,1 +1,60 @@
 package stringliterals
+
+import (
+	"minimal/minimal-core/built-in/tokenizer"
+	"minimal/minimal-core/domain"
+	"strings"
+)
+
+type StringLiteralMatcher struct {
+	tokenType domain.TokenType
+}
+
+func NewStringLiteralMatcher(tt domain.TokenType) StringLiteralMatcher {
+	return StringLiteralMatcher{tt}
+}
+
+func (s *StringLiteralMatcher) Match(so *tokenizer.Source) (uint, domain.TokenType, string) {
+	firstChar, _ := so.Get(0)
+
+	if !(firstChar == '"') {
+		return 0, domain.IGNORE, ""
+	}
+
+	sb := strings.Builder{}
+
+	pos := 1
+	for {
+		currentChar, ok := so.Get(pos)
+
+		if !ok {
+			// TODO log error
+			panic("No closing character \"")
+		}
+
+		if currentChar == '"' {
+			pos++
+			break;
+		}
+
+		if currentChar == '\\' {
+			sb.WriteByte(currentChar)
+			// The characters that can close a string are all one byte long
+			charToSkip, ok := so.Get(pos + 1)
+
+			if !ok {
+				// TODO log error
+				panic("Nothing to escape")
+			}
+
+			sb.WriteByte(charToSkip)
+
+			pos += 2
+		} else {
+			sb.WriteByte(currentChar)
+			pos++
+		}
+	}
+
+	return uint(pos), s.tokenType, sb.String()
+}
---
+++ built-in/extensions/matchers/string_literals/string_literals_test.go
@@ -0,0 +1,58 @@
+package stringliterals
+
+import (
+	"fmt"
+	"minimal/minimal-core/built-in/tokenizer"
+	"minimal/minimal-core/domain"
+	"reflect"
+	"testing"
+)
+
+func TestLexStringLiteral(t *testing.T) {
+	config := tokenizer.NewTokenizerConfig()
+	stringLiteralType := config.NewTokenType()
+	stringLiteralMatcher := NewStringLiteralMatcher(stringLiteralType)
+
+	config.AddMatcher(&stringLiteralMatcher)
+
+	expected := []domain.Token{
+		{Type: stringLiteralType, Value: "Hello, world!", Span: domain.Span{Start: 0, Length: 15}},
+		{Type: domain.EOF, Value: "", Span: domain.Span{Start: 15, Length: 0}},
+	}
+
+	actual := tokenizer.NewTokenizer(config, []byte("\"Hello, world!\""))
+
+	i := 0
+	for ; actual.CurrentToken().Type != domain.EOF; i++ {
+		if i >= len(expected) {
+			t.Fatal("Expected", len(expected), "tokens but got", i+1, "tokens")
+		}
+
+		if !reflect.DeepEqual(actual.CurrentToken(), expected[i]) {
+			t.Error("Expected", expected[i], "but got", actual.CurrentToken())
+		}
+
+		actual.Consume()
+	}
+
+	if i+1 != len(expected) {
+		t.Fatal("Expected", len(expected), "tokens but got", i+1, "tokens")
+	}
+}
+
+func TestLexUnclosed(t *testing.T) {
+	config := tokenizer.NewTokenizerConfig()
+	stringLiteralType := config.NewTokenType()
+	stringLiteralMatcher := NewStringLiteralMatcher(stringLiteralType)
+
+	config.AddMatcher(&stringLiteralMatcher)
+
+	// TODO check if it logs properly
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	tokenizer.NewTokenizer(config, []byte("\"Hello, world!"))
+}
