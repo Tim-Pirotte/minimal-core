349fef64a85791aa4e828d264a44ca0de1139b39
Tim Pirotte <timpirotte@outlook.com>
2026-03-01 11:51:43 +0100

    Allows changing the current directory

--- built-in/extensions/user-interfaces/tui/tui.go
+++ built-in/extensions/user-interfaces/tui/tui.go
@@ -3,7 +3,9 @@ package tui
 import (
 	"errors"
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/gdamore/tcell/v2"
@@ -15,10 +17,16 @@ var ErrDuplicateShellCommand = errors.New("shell command with this name already 
 type TUI struct {
 	app            *tview.Application
 	actions        *tview.List
-	shellOutput    *tview.TextView
-	shellCommands  map[string]func(args []string)
-	commandHistory []string
-	historyIndex   int
+	shell          shell
+}
+
+type shell struct {
+	input            *tview.InputField
+	output           *tview.TextView
+	commands         map[string]func(args []string)
+	history          []string
+	historyIndex     int
+	initialDirectory string
 }
 
 type Action struct {
@@ -29,13 +37,19 @@ type Action struct {
 }
 
 func NewTUI() *TUI {
+	initialDirectory, _ := os.Getwd()
+
 	tui := &TUI{
 		tview.NewApplication(), 
 		tview.NewList(), 
-		tview.NewTextView(),
-		make(map[string]func(args []string)), 
-		make([]string, 0), 
-		0,
+		shell{
+			tview.NewInputField(),
+			tview.NewTextView(),
+			make(map[string]func(args []string)), 
+			make([]string, 0), 
+			0,
+			initialDirectory,
+		},
 	}
 	
 	tui.AddAction(Action{"Run", "Run the project", 'r', nil})
@@ -46,7 +60,36 @@ func NewTUI() *TUI {
 	}})
 
 	tui.AddShellCommand("clear", func(args []string) {
-		tui.shellOutput.Clear()
+		tui.shell.output.Clear()
+	})
+
+	tui.AddShellCommand("cd", func(args []string) {
+		if len(args) < 2 {
+			err := os.Chdir(tui.shell.initialDirectory)
+
+			if err != nil {
+				tui.shell.output.Write([]byte(err.Error()))
+				return
+			}
+
+			tui.shell.input.SetLabel(tui.shell.initialDirectory + " |> ")
+		} else {
+			path := args[1]
+
+			if strings.HasPrefix(path, "~") {
+				home, _ := os.UserHomeDir()
+				path = filepath.Join(home, path[1:])
+			}
+
+			err := os.Chdir(path)
+
+			if err != nil {
+				tui.shell.output.Write([]byte(err.Error()))
+				return
+			}
+
+			tui.shell.input.SetLabel(path + " |> ")
+		}
 	})
 
 	return tui
@@ -57,11 +100,11 @@ func (t *TUI) AddAction(action Action) {
 }
 
 func (t *TUI) AddShellCommand(name string, run func(args []string)) error {
-	if _, ok := t.shellCommands[name]; ok {
+	if _, ok := t.shell.commands[name]; ok {
 		return ErrDuplicateShellCommand
 	}
 	
-	t.shellCommands[name] = run
+	t.shell.commands[name] = run
 	
 	return nil
 }
@@ -93,39 +136,39 @@ func (t *TUI) setDashBoard() *tview.Grid {
 	output := tview.NewTextView()
 	output.SetBorder(true).SetTitle("Output")
 
-	t.shellOutput.SetChangedFunc(func() { t.app.Draw() })
-	t.shellOutput.SetScrollable(true)
-	t.shellOutput.SetDynamicColors(true)
+	t.shell.output.SetChangedFunc(func() { t.app.Draw() })
+	t.shell.output.SetScrollable(true)
+	t.shell.output.SetDynamicColors(true)
 
-	shellInput := tview.NewInputField().SetLabel(":> ")
-	shellInput.SetDoneFunc(
+	t.shell.input.SetLabel("|  ")
+	t.shell.input.SetDoneFunc(
 		func(key tcell.Key) {
 			if key != tcell.KeyEnter {
 				return
 			}
 
-			commandText := shellInput.GetText()
-			shellInput.SetText("")
+			commandText := t.shell.input.GetText()
+			t.shell.input.SetText("")
 
 			if commandText == "" {
 				return
 			}
 
-			t.commandHistory = append(t.commandHistory, commandText)
-			t.historyIndex = len(t.commandHistory)
+			t.shell.history = append(t.shell.history, commandText)
+			t.shell.historyIndex = len(t.shell.history)
 
-			fmt.Fprintf(t.shellOutput, "\n|> %s\n", commandText)
+			fmt.Fprintf(t.shell.output, "\n|> %s\n", commandText)
 
 			parts := strings.Fields(commandText)
 
 			if len(parts) > 0 {
-				if run, ok := t.shellCommands[parts[0]]; ok {
+				if run, ok := t.shell.commands[parts[0]]; ok {
 					run(parts)
 					return
 				}
 			}
 
-			ansiWriter := tview.ANSIWriter(t.shellOutput)
+			ansiWriter := tview.ANSIWriter(t.shell.output)
 			cmd := exec.Command(parts[0], parts[1:]...)
 			cmd.Stdout = ansiWriter
         	cmd.Stderr = ansiWriter
@@ -137,48 +180,48 @@ func (t *TUI) setDashBoard() *tview.Grid {
 					fmt.Fprint(ansiWriter, err)
 				}
 
-				t.shellOutput.ScrollToEnd()
+				t.shell.output.ScrollToEnd()
 			}()
 		},
 	)
 
-	shellInput.SetLabelColor(tcell.ColorWhite)
+	t.shell.input.SetLabelColor(tcell.ColorWhite)
 
-	shellInput.SetFocusFunc(func() {
-		shellInput.SetLabelColor(tcell.ColorRed)
-		shellInput.SetLabel("|> ")
+	t.shell.input.SetFocusFunc(func() {
+		t.shell.input.SetLabelColor(tcell.ColorRed)
+		t.shell.input.SetLabel("|> ")
 	})
 
-	shellInput.SetBlurFunc(func() {
-		shellInput.SetLabelColor(tcell.ColorWhite)
-		shellInput.SetLabel(":> ")
+	t.shell.input.SetBlurFunc(func() {
+		t.shell.input.SetLabelColor(tcell.ColorWhite)
+		t.shell.input.SetLabel("|  ")
 	})
 
 	shell := tview.NewFlex().
 		SetDirection(tview.FlexRow).
-		AddItem(t.shellOutput, 0, 1, false).
-		AddItem(shellInput, 1, 0, false)
+		AddItem(t.shell.output, 0, 1, false).
+		AddItem(t.shell.input, 1, 0, false)
 	
 	shell.SetBorder(true).SetTitle("Shell")
 
-	shellInput.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	t.shell.input.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
         case tcell.KeyUp:
-            if t.historyIndex > 0 {
-				t.historyIndex--
-				shellInput.SetText(t.commandHistory[t.historyIndex])
+            if t.shell.historyIndex > 0 {
+				t.shell.historyIndex--
+				t.shell.input.SetText(t.shell.history[t.shell.historyIndex])
 			}
 
             return nil
         case tcell.KeyDown:
-			if t.historyIndex < len(t.commandHistory) {
-				t.historyIndex++
+			if t.shell.historyIndex < len(t.shell.history) {
+				t.shell.historyIndex++
 			}
 
-			if t.historyIndex < len(t.commandHistory) {
-				shellInput.SetText(t.commandHistory[t.historyIndex])
+			if t.shell.historyIndex < len(t.shell.history) {
+				t.shell.input.SetText(t.shell.history[t.shell.historyIndex])
 			} else {
-				shellInput.SetText("")
+				t.shell.input.SetText("")
 			}
             
 			return nil
@@ -197,7 +240,7 @@ func (t *TUI) setDashBoard() *tview.Grid {
 
 	dashBoard.SetBorder(true).SetTitle("Minimal")
 
-	t.setPanelNavigation([]tview.Primitive{t.actions, output, shellInput, t.shellOutput})
+	t.setPanelNavigation([]tview.Primitive{t.actions, output, t.shell.input, t.shell.output})
 
 	return dashBoard
 }
