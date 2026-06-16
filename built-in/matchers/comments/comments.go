7569a726a6194df33352a8d2b8922da1da0eec24
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-16 20:28:52 +0200

    Adds a matcher for comments

--- built-in/matchers/comments/comments.go
+++ built-in/matchers/comments/comments.go
@@ -1,1 +1,43 @@
 package comments
+
+import (
+	"minimal/minimal-core/built-in/lexer"
+	"minimal/minimal-core/built-in/primitives"
+)
+
+const prefix = '|'
+
+type CommentMatcher struct {
+	tokenType lexer.TokenType
+}
+
+func NewCommentMatcher(tt lexer.TokenType) *CommentMatcher {
+	return &CommentMatcher{tt}
+}
+
+func (n *CommentMatcher) Match(t *lexer.LexerJob) uint {
+	start, _ := t.Get(0)
+
+	if start != prefix {
+		return 0
+	}
+
+	pos := uint(1)
+
+	// Keep these EOL sequences with the EOL matcher
+	for c, ok := t.Get(pos); ok && c != '\n' && c != '\r'; c, ok = t.Get(pos) {
+		pos++
+	}
+
+	return pos
+}
+
+func (i *CommentMatcher) Consume(t *lexer.LexerJob, length uint) {
+	comment, _ := t.GetRange(t.Position, length)
+
+	t.Emit(lexer.Token{
+		Type:  i.tokenType,
+		Value: comment,
+		Range: primitives.Range{Start: t.Position, Length: length}},
+	)
+}
--- built-in/matchers/end-of-line/end_of_line.go
+++ built-in/matchers/end-of-line/end_of_line.go
@@ -18,6 +18,7 @@ func (*EOLMatcher) Match(t *lexer.LexerJob) uint {
 
 	// I have decided that for now we only allow LF and CR as end of line sequences
 	// Since I don't know a good reason to include NEL, LS and PS
+	// Keep these EOL sequences with the comment matcher
 	for c, ok := t.Get(pos); ok && (c == '\n' || c == '\r'); c, ok = t.Get(pos) {
 		pos++
 	}
