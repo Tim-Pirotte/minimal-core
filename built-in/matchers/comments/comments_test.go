c6a00447887a948fc2128bd6f640dbd75beb2c48
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-16 21:48:12 +0200

    Adds tests for comments

--- built-in/matchers/comments/comments_test.go
+++ built-in/matchers/comments/comments_test.go
@@ -2,6 +2,7 @@ package comments
 
 import (
 	"minimal/minimal-core/built-in/lexer"
+	"minimal/minimal-core/built-in/primitives"
 	"testing"
 )
 
@@ -18,5 +19,25 @@ func getLexer() (*lexer.Lexer, lexer.TokenType) {
 }
 
 func TestComments(t *testing.T) {
+	l, commentType := getLexer()
 
+	expected := []lexer.Token{
+		{Type: lexer.UNKNOWN, Value: "0", Range: primitives.Range{Start: 0, Length: 1}},
+		{Type: commentType, Value: "| A comment", Range: primitives.Range{Start: 1, Length: 11}},
+	}
+
+	lexer.CheckTokens(t, l, expected, "0| A comment")
+}
+
+func TestNewLine(t *testing.T) {
+	l, commentType := getLexer()
+
+	expected := []lexer.Token{
+		{Type: commentType, Value: "| A comment", Range: primitives.Range{Start: 0, Length: 11}},
+		{Type: lexer.UNKNOWN, Value: "\n", Range: primitives.Range{Start: 11, Length: 1}},
+		{Type: commentType, Value: "| Second comment", Range: primitives.Range{Start: 12, Length: 16}},
+		{Type: lexer.UNKNOWN, Value: "\r", Range: primitives.Range{Start: 28, Length: 1}},
+	}
+
+	lexer.CheckTokens(t, l, expected, "| A comment\n| Second comment\r")
 }
