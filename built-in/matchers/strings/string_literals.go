fa2c7f87270e99443b55b484c634c1a410582d70
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-07-02 20:00:53 +0200

    Adds unclosed string error handling

--- built-in/matchers/strings/string_literals.go
+++ built-in/matchers/strings/string_literals.go
@@ -1,40 +1,69 @@
 package strings
 
-import "minimal/minimal-core/built-in/lexer"
+import (
+	"minimal/minimal-core/built-in/lexer"
+	"minimal/minimal-core/built-in/matchers/indentation"
+	"minimal/minimal-core/built-in/messaging"
+)
 
 type StringMatcher struct {
-	tokenType lexer.TokenType
+    messenger *messaging.Messenger
+    tokenType lexer.TokenType
 }
 
-func NewStringMatcher(tt lexer.TokenType) *StringMatcher {
-	return &StringMatcher{tt}
+func NewStringMatcher(messenger *messaging.Messenger, tt lexer.TokenType) *StringMatcher {
+    return &StringMatcher{messenger, tt}
 }
 
 func (s *StringMatcher) New(_ *lexer.LexerJob) lexer.Matcher {
-	return s
+    return s
 }
 
 func (s *StringMatcher) Match(l *lexer.LexerJob) uint {
-	firstChar, _ := l.Get(0)
+    firstChar, _ := l.Get(0)
 
-	if firstChar != '"' {
-		return 0
-	}
+    if firstChar != '"' {
+        return 0
+    }
 
-	pos := uint(1)
-	c, ok := l.Get(pos)
+    pos := uint(1)
+    c, ok := l.Get(pos)
 
-	for ; ok && c != '"'; c, ok = l.Get(pos) {
-		pos++
-	}
+    for ; ok && c != '"'; c, ok = l.Get(pos) {
+        if c == '\\' {
+            pos += 2
+        } else {
+            pos++
+        }
+    }
 
-	if !ok {
-		// TODO error no closing and only consume up to the next newline
-	}
+    if !ok {
+        s.sendUnclosedStrErr(l)
 
-	return pos
+        pos = 1
+
+        for c, ok = l.Get(pos); ok && !indentation.IsEOL(c); c, ok = l.Get(pos) {
+            pos++
+        }
+
+        return pos
+    }
+
+    return pos
 }
 
 func (s *StringMatcher) Consume(l *lexer.LexerJob, length uint) {
-	l.Emit(lexer.Token{Type: s.tokenType, Value: l.GetNextN(length)})
+    l.Emit(lexer.Token{Type: s.tokenType, Value: l.GetNextN(length)})
+}
+
+func (s *StringMatcher) sendUnclosedStrErr(l *lexer.LexerJob) {
+    s.messenger.Send(messaging.Message{
+        Reference: "TODO",
+        Message: `String is not terminated with a "`,
+        Severity: messaging.Error,
+        Context: []messaging.Span{
+            { Content: l.GetNextN(1), Note: "The string starts here"},
+        },
+        Notes: []string{"The current line will be skipped"},
+    })
 }
