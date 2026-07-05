cd605924cfe03ce0210694f7664500381f56ac92
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-07-05 13:23:20 +0200

    Improves the string lexing

--- built-in/matchers/strings/string_literals.go
+++ built-in/matchers/strings/string_literals.go
@@ -5,31 +5,18 @@ import (
 	"minimal/minimal-core/built-in/messaging"
 )
 
-var interpolation EnclosingSet = EnclosingSet{
-    openSequence: "{",
-    closingSequence: "}",
-}
-
 type StringMatcher struct {
     messenger *messaging.Messenger
+    lexer     *lexer.Lexer
     tokenType lexer.TokenType
-    enclosingSets []EnclosingSet
-}
-
-// TODO assert length > 1
-type EnclosingSet struct {
-    openSequence    string
-    closingSequence string
 }
 
 func NewStringMatcher(
     messenger *messaging.Messenger,
+    lexer *lexer.Lexer,
     tt lexer.TokenType,
-    enclosingSets []EnclosingSet,
 ) *StringMatcher {
-    enclosingSets = append(enclosingSets, interpolation)
-
-    return &StringMatcher{messenger, tt, enclosingSets}
+    return &StringMatcher{messenger, lexer, tt}
 }
 
 func (s *StringMatcher) New(_ *lexer.LexerJob) lexer.Matcher {
@@ -37,49 +24,29 @@ func (s *StringMatcher) New(_ *lexer.LexerJob) lexer.Matcher {
 }
 
 func (s *StringMatcher) Match(l *lexer.LexerJob) uint {
-    nesting := []EnclosingSet{}
-
     firstChar, _ := l.Get(0)
 
-    if firstChar != '"' {
+    if firstChar == '"' {
+        return 1
+    } else {
         return 0
     }
+}
 
+func (s *StringMatcher) Consume(l *lexer.LexerJob, length uint) {
     pos := uint(1)
     c, ok := l.Get(pos)
 
-    for ; ok && (c != '"' || len(nesting) != 0); c, ok = l.Get(pos) {
-        if len(nesting) > 0 {
-            firstToClose := nesting[len(nesting) - 1].closingSequence
-            maybeClose := l.Data[l.Position + pos:l.Position + pos + uint(len(firstToClose))]
-
-            if maybeClose == firstToClose {
-                nesting = nesting[:len(nesting) - 1]
-                pos += uint(len(firstToClose))
-            } else {
-                found := false
-
-                for _, s := range s.enclosingSets {
-                    maybeOpen := l.Data[l.Position + pos:l.Position + pos + uint(len(s.openSequence))]
-
-                    if maybeOpen == s.openSequence {
-                        nesting = append(nesting, s)
-                        pos += uint(len(s.openSequence))
-                        found = false
-                        break
-                    }
-                }
-
-                if !found {
-                    pos++
-                }
+    for ; ok && c != '"'; c, ok = l.Get(pos) {
+        switch c {
+        case '{':
+            // TODO Lex and offset position
+            if c, ok := l.Get(0); !ok || c != '}' {
+                // TODO Error: interpolation not properly closed
             }
-        } else if c == '\\' {
+        case '\\':
             pos += 2
-        } else if c == '{' {
-            nesting = append(nesting, interpolation)
-            pos++
-        } else {
+        default:
             pos++
         }
     }
@@ -87,15 +54,8 @@ func (s *StringMatcher) Match(l *lexer.LexerJob) uint {
     if !ok {
         s.sendUnclosedStrErr(l)
 
-        return pos
+        l.Emit(lexer.Token{Type: s.tokenType, Value: l.GetNextN(pos)})
     }
-
-    // To include the ending quote
-    return pos + 1
-}
-
-func (s *StringMatcher) Consume(l *lexer.LexerJob, length uint) {
-    l.Emit(lexer.Token{Type: s.tokenType, Value: l.GetNextN(length)})
 }
 
 func (s *StringMatcher) sendUnclosedStrErr(l *lexer.LexerJob) {
