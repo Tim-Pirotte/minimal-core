6250a810a0c50ce791404941947609c30373bfd8
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-07-06 21:23:03 +0200

    Adds setup for raw string tests

--- built-in/matchers/raw-string/raw_string_test.go
+++ built-in/matchers/raw-string/raw_string_test.go
@@ -1,6 +1,45 @@
 package rawstring
 
-import "testing"
+import (
+	"minimal/minimal-core/built-in/lexer"
+	"minimal/minimal-core/built-in/messaging"
+	logrendering "minimal/minimal-core/built-in/outputs/log-renderer"
+	"os"
+	"testing"
+)
+
+type testLexer struct {
+    l         *lexer.Lexer
+    strType   lexer.TokenType
+    messenger *messaging.Messenger
+    output    *messaging.TestOutput
+}
+
+func getLexer() testLexer {
+    l := lexer.NewLexer()
+
+    rawStringType := l.NewTokenType(
+        lexer.TokenTypeMetadata{DisplayName: "a raw string literal", DebugName: "RawString"},
+    )
+
+    messenger := messaging.NewMessenger()
+    logrenderer := logrendering.NewLogRenderer(os.Stdout)
+    logrenderer.Config.RemoveANSI()
+    logrenderer.Config.RemoveUnicode()
+    messenger.AddOutput(logrenderer)
+
+    testOutput := &messaging.TestOutput{}
+    messenger.AddOutput(testOutput)
+
+    stringMatcher := NewRawStringMatcher(
+        messenger,
+        rawStringType,
+    )
+
+    l.AddMatcher(stringMatcher)
+
+    return testLexer{l, rawStringType, messenger, testOutput}
+}
 
 func TestRawString(t *testing.T) {
 
