e3c2084dde6ecb0dac63dc6f884d4912618bc882
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-07-28 14:07:41 +0200

    Adds a unary postfix parser

---
+++ built-in/parsers/postfix-unary/postfix_unary.go
@@ -0,0 +1,38 @@
+package postfixunary
+
+import (
+	"minimal/minimal-core/built-in/ast"
+	"minimal/minimal-core/built-in/lexer"
+	"minimal/minimal-core/built-in/parsers/pratt"
+)
+
+type PostfixUnaryParser struct {
+	tokenType    lexer.TokenType
+	nodeType     ast.NodeType
+	bindingPower uint
+}
+
+func NewPostfixUnaryParser(
+    tokenType lexer.TokenType, nodeType ast.NodeType, bindingPower uint,
+) *PostfixUnaryParser {
+	return &PostfixUnaryParser{tokenType, nodeType, bindingPower}
+}
+
+func (p *PostfixUnaryParser) GetTokenType() lexer.TokenType {
+	return p.tokenType
+}
+
+func (p *PostfixUnaryParser) GetBindingPower() uint {
+	return p.bindingPower
+}
+
+func (p *PostfixUnaryParser) ParseInfix(
+	_ *pratt.PrattParser, l *lexer.LexerJob, left []ast.Node, minBindingPower uint,
+) []ast.Node {
+	l.Advance()
+
+	result := []ast.Node{{Type: p.nodeType}}
+	result = append(result, left...)
+
+	return result
+}
---
+++ built-in/parsers/postfix-unary/postfix_unary_test.go
@@ -0,0 +1,68 @@
+package postfixunary
+
+import (
+	"minimal/minimal-core/built-in/ast"
+	"minimal/minimal-core/built-in/lexer"
+	symbols "minimal/minimal-core/built-in/matchers/symbol"
+	"minimal/minimal-core/built-in/parsers/binary"
+	"minimal/minimal-core/built-in/parsers/pratt"
+	"reflect"
+	"testing"
+)
+
+func TestBindingPower(t *testing.T) {
+    l := lexer.NewLexer()
+
+    aT := l.NewTokenType(lexer.TokenTypeMetadata{DebugName: "a"})
+    bT := l.NewTokenType(lexer.TokenTypeMetadata{DebugName: "b"})
+
+    plusT := l.NewTokenType(lexer.TokenTypeMetadata{DebugName: "+"})
+    exclamationT := l.NewTokenType(lexer.TokenTypeMetadata{DebugName: "!"})
+    quoteT := l.NewTokenType(lexer.TokenTypeMetadata{DebugName: "'"})
+
+    sm := symbols.NewSymbolMatcher()
+    sm.AddSymbol(l, "a", aT)
+    sm.AddSymbol(l, "b", bT)
+
+    sm.AddSymbol(l, "+", plusT)
+    sm.AddSymbol(l, "!", exclamationT)
+    sm.AddSymbol(l, "'", quoteT)
+
+    l.AddMatcher(sm)
+
+    syntax := ast.NewAst()
+    plus := syntax.NewNodeType(ast.NodeTypeMetadata{DebugName: "+"})
+    exclamation := syntax.NewNodeType(ast.NodeTypeMetadata{DebugName: "!"})
+    quote := syntax.NewNodeType(ast.NodeTypeMetadata{DebugName: "'"})
+
+    a := syntax.NewNodeType(ast.NodeTypeMetadata{DebugName: "a"})
+    b := syntax.NewNodeType(ast.NodeTypeMetadata{DebugName: "b"})
+
+    p := pratt.NewPrattParser(
+        l,
+        []pratt.Prefix{
+            pratt.NewAtomicParser(aT, a),
+            pratt.NewAtomicParser(bT, b),
+        },
+        []pratt.Infix{
+            binary.NewBinaryParser(plusT, plus, 2),
+            NewPostfixUnaryParser(exclamationT, exclamation, 2),
+            NewPostfixUnaryParser(quoteT, quote, 1),
+        },
+    )
+
+    lj := l.Lex("a!+b'", 1)
+    result := p.Parse(lj, 0)
+
+    expected := []ast.Node{
+        {Type: quote},
+        {Type: plus},
+        {Type: exclamation},
+        {Type: a},
+        {Type: b},
+    }
+
+    if !reflect.DeepEqual(expected, result) {
+        t.Errorf("\nExpected:\n%v\nActual:\n%v", expected, result)
+    }
+}
