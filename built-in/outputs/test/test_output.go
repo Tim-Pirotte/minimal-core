0a086c5b176aecd3bf9f489c1aa38ab975eaad5d
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-09-02 12:52:30 +0200

    Checks if strings in the messages for things like context come from the same place as expected

--- built-in/outputs/test/test_output.go
+++ built-in/outputs/test/test_output.go
@@ -1,8 +1,10 @@
 package testoutput
 
 import (
+	"fmt"
 	"minimal/minimal-core/built-in/messenger"
 	logrendering "minimal/minimal-core/built-in/outputs/log-renderer"
+	"minimal/minimal-core/built-in/substring"
 	"os"
 	"reflect"
 	"testing"
@@ -30,7 +32,6 @@ func (to *TestOutput) CheckMessages(t *testing.T, expected []messenger.Message) 
     m := messenger.New()
     m.AddOutput(to.Output)
 
-    // TODO check if the context has the correct addresses
     if !reflect.DeepEqual(to.messages, expected) {
         m.Send(
             messenger.Message{
@@ -51,7 +52,63 @@ func (to *TestOutput) CheckMessages(t *testing.T, expected []messenger.Message) 
             m.Send(message)
         }
 
-        m.Close()
+        t.Fail()
+    } else {
+        for i := range expected {
+            for j := range expected[i].Context {
+                compareSubStrings(
+                    m,
+                    t,
+                    expected[i].Context[j].Content,
+                    to.messages[i].Context[j].Content,
+                    expected[i].Message,
+                    i + 1,
+                )
+            }
+
+            for j := range expected[i].AdditionalContext {
+                compareSubStrings(
+                    m,
+                    t,
+                    expected[i].AdditionalContext[j].Content,
+                    to.messages[i].AdditionalContext[j].Content,
+                    expected[i].Message,
+                    i + 1,
+                )
+            }
+
+            for j := range expected[i].Suggestions {
+                for k := range expected[i].Suggestions[j].Replacements {
+                    compareSubStrings(
+                        m,
+                        t,
+                        expected[i].Suggestions[j].Replacements[k].From.Content,
+                        to.messages[i].Suggestions[j].Replacements[k].From.Content,
+                        expected[i].Message,
+                        i + 1,
+                    )
+                }
+            }
+        }
+    }
+
+    m.Close()
+}
+
+func compareSubStrings(m *messenger.Messenger, t *testing.T, expected, actual, message string, number int) {
+    if !substring.IsSubString(expected, actual) {
+        m.Send(
+            messenger.Message{
+                Message: "Different address than expected",
+                Severity: messenger.Error,
+                Notes: []string{
+                    fmt.Sprintf("Message number: %d", number),
+                    fmt.Sprintf("Message text: %s", message),
+                    fmt.Sprintf("Content: %s", expected),
+                },
+            },
+        )
+
         t.Fail()
     }
 }
--- built-in/substring/substring.go
+++ built-in/substring/substring.go
@@ -3,13 +3,16 @@ package substring
 import "unsafe"
 
 func GetStringPtr(str string) uintptr {
-	return uintptr(unsafe.Pointer(unsafe.StringData(str)))
+    return uintptr(unsafe.Pointer(unsafe.StringData(str)))
 }
 
 // Checks if a strings data is a subset of another strings data
 func IsSubString(str, possibleSubStr string) bool {
-	strPtr := GetStringPtr(str)
-	subStrPtr := GetStringPtr(possibleSubStr)
+    strPtr := GetStringPtr(str)
+    subStrPtr := GetStringPtr(possibleSubStr)
 
-	return strPtr <= subStrPtr && subStrPtr < strPtr + uintptr(len(str))
+    strEnd := strPtr + uintptr(len(str))
+	subStrEnd := subStrPtr + uintptr(len(possibleSubStr))
+
+    return strPtr <= subStrPtr && subStrEnd <= strEnd
 }
